e5670112504dece362ae4404015da93d817108e3
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-06-21 15:09:43 +0200

    Fix CS/WS issues

--- src/Command.php
+++ src/Command.php
@@ -13,15 +13,9 @@ namespace SebastianBergmann\phptok;
 use Symfony\Component\Console\Command\Command as AbstractCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Helper\ProgressBar;
 
 /**
- * @author    Sebastian Bergmann <sebastian@phpunit.de>
- * @copyright Sebastian Bergmann <sebastian@phpunit.de>
- * @license   http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
- * @link      http://github.com/sebastianbergmann/phptok/tree
  * @since     Class available since Release 1.0.0
  */
 class Command extends AbstractCommand
@@ -48,7 +42,7 @@ class Command extends AbstractCommand
      * @param InputInterface  $input  An InputInterface instance
      * @param OutputInterface $output An OutputInterface instance
      *
-     * @return null|integer null or 0 if everything went fine, or an error code
+     * @return null|int null or 0 if everything went fine, or an error code
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
@@ -62,7 +56,7 @@ class Command extends AbstractCommand
             if ($token instanceof \PHP_Token_WHITESPACE) {
                 $text = '';
             } else {
-                $text = str_replace(array("\r", "\n"), '', (string)$token);
+                $text = str_replace(array("\r", "\n"), '', (string) $token);
 
                 if (strlen($text) > 40) {
                     $text = explode("\n", wordwrap($text, 40));
@@ -72,7 +66,7 @@ class Command extends AbstractCommand
 
             $output->writeln(
                 sprintf(
-                    "%5d  %-30s %s",
+                    '%5d  %-30s %s',
                     $token->getLine(),
                     str_replace('PHP_Token_', '', get_class($token)),
                     $text
